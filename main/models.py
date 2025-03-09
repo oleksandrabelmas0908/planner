4a30dbd13c74fb34f7926599cd1d92e7e905a89c
oleksandrabelmas0908 <oleksandr.abelmas0908@gmail.com>
2025-03-09 13:48:55 +0100

    create models and set up the project

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+
+class Tasks(models.Model):
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    time_it_takes = models.CharField(max_length=20)
+    is_done = models.BooleanField(default=False)
