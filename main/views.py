2c732114f4aef0c79877003cc039c5ec407264e6
Oleksandr Abelmas <oleksandr.abelmas@gmail.com>
2025-03-14 22:06:34 +0100

    Added a main page

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
 
 def index(req):
-    return HttpResponse("hello world")
+    return render(template_name="index.html", request=req)
+
+
